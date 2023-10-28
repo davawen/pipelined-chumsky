6ea8d7d76ba67b368ff3558e5f60286db292e18e
davawen <davawen@proton.me>
2023-10-28 13:08:13 +0200

    Implement lexer/parser pair

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,89 @@
+#[derive(Debug, Clone, Copy, PartialEq)]
+enum Token<'a> {
+    Number(f32),
+    String(&'a str),
+    Ident(&'a str),
+    Lparen,
+    Rparen,
+    Comma,
+    Pipeline,
+    Assign,
+    Mutate,
+    Lambda
+}
+
+impl std::fmt::Display for Token<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        use Token as T;
+        match self {
+            T::Number(x) => write!(f, "{x}"),
+            T::String(x) => write!(f, "{x:?}"),
+            T::Ident(x) => write!(f, "{x}"),
+            T::Lparen => write!(f, "("),
+            T::Rparen => write!(f, ")"),
+            T::Comma => write!(f, ","),
+            T::Pipeline => write!(f, "|>"),
+            T::Assign => write!(f, "->"),
+            T::Mutate => write!(f, "->>"),
+            T::Lambda => write!(f, "=>"),
+        }
+    }
+}
+
+fn lexer<'a>() -> impl Parser<'a, &'a str, Vec<(Token<'a>, SimpleSpan)>, extra::Err<Rich<'a, char>>> {
+    let number = text::digits(10)
+        .then(just('.').then(text::digits(10)).or_not())
+        .to_slice()
+        .map(|s: &str| Token::Number(s.parse().unwrap()));
+
+    /// using a closure complains about lifetimes
+    macro_rules! symbol {
+        ($c:literal, $t:expr) => {
+           just($c).map(|_| $t)
+        };
+    }
+
+    let symbols = choice((
+        symbol!("->>", Token::Mutate),
+        symbol!("->", Token::Assign),
+        symbol!("=>", Token::Lambda),
+        symbol!("|>", Token::Pipeline),
+        symbol!("(", Token::Lparen),
+        symbol!(")", Token::Rparen),
+        symbol!(",", Token::Comma)
+    ));
+
+    let comment = just("--")
+        .then(any().and_is(just("\n").not()).repeated())
+        .padded();
+
+    let allowed_ident = symbols.clone().not().rewind()
+        .then_ignore(comment.not().rewind())
+        .then(none_of(" \t\n\r"));
+
+    let ident = text::digits(10).not().rewind()
+        .then(allowed_ident.clone())
+        .then(allowed_ident.repeated())
+        .to_slice()
+        .map(|s: &str| Token::Ident(s));
+
+    let string = just('"')
+        .ignore_then(none_of("\"").repeated().to_slice())
+        .then_ignore(just('"'))
+        .map(Token::String);
+
+    let token = choice((number, string, ident, symbols));
+
+    token
+        .map_with(|t, e| (t, e.span()))
+        .padded_by(comment.repeated())
+        .padded()
+        .recover_with(skip_then_retry_until(any().ignored(), end()))
+        .repeated()
+        .collect()
+        .then_ignore(end())
+}
+
 #[derive(Debug)]
 pub enum Expr {
     Ident(String),
@@ -10,69 +96,61 @@ pub enum Expr {
     Lambda(Vec<String>, Box<Expr>)
 }
 
-use ariadne::{Report, ReportKind, Label, Color, sources};
-use chumsky::{prelude::*, input::StrInput, pratt::{self, left, infix, postfix, prefix}};
+use std::fmt::Display;
 
-fn parser<'a>() -> impl Parser<'a, &'a str, Expr, chumsky::extra::Err<Rich<'a, char>>> {
-    // let p = just("--")
-    //     .then(none_of("\n").repeated())
-    //     .then(just('\n'))
-    //     .padded
-    //     .or();
+use chumsky::{prelude::*, input::{StrInput, SpannedInput}, pratt::{self, left, infix, postfix, prefix}};
 
-    let number = text::digits(10)
-        .then(just('.').then(text::digits(10)).or_not())
-        .to_slice()
-        .map(|s: &str| Expr::Number(s.parse().unwrap()));
+fn parser<'a>() -> impl Parser<'a, SpannedInput<Token<'a>, SimpleSpan, &'a [(Token<'a>, SimpleSpan)]>, Expr, chumsky::extra::Err<Rich<'a, Token<'a>>>> {
+    let value = select !{
+        Token::Number(x) => Expr::Number(x),
+        Token::String(x) => Expr::String(x.to_owned())
+    };
 
-    let no = |x| just(x).not().rewind();
-
-    let allowed_ident = no("->>").ignored()
-        .then_ignore(no("->"))
-        .then_ignore(no("=>"))
-        .then_ignore(no("|>"))
-        .then_ignore(no("--"))
-        .then(none_of("(), \t\n\r"));
-
-    let ident = text::digits(10).not().rewind()
-        .then(allowed_ident)
-        .then(allowed_ident.repeated())
-        .to_slice()
-        .map(|s: &str| s.to_owned());
-
-    let string = just('"')
-        .ignore_then(none_of("\"").repeated().collect::<String>())
-        .then_ignore(just('"'))
-        .map(Expr::String);
+    let ident = select! { Token::Ident(x) => x.to_owned() };
+    let eident = ident.map(Expr::Ident);
 
     recursive(|expr| {
-        let value = string.or(number);
-
         let tuple = value.or(expr.clone())
-            .padded().separated_by(just(','))
+            .separated_by(just(Token::Comma))
             .collect::<Vec<_>>()
-            .delimited_by(just('('), just(')'))
+            .delimited_by(just(Token::Lparen), just(Token::Rparen))
             .map(Expr::Tuple);
 
-        let standalone = ident.map(Expr::Ident).or(tuple);
+        let standalone = eident.or(tuple);
 
         let arg_list = ident
-            .padded().separated_by(just(','))
+            .separated_by(just(Token::Comma))
             .collect::<Vec<_>>()
-            .delimited_by(just('('), just(')'));
-
-        let op = |c| just(c).padded();
+            .delimited_by(just(Token::Lparen), just(Token::Rparen));
 
         standalone
             .pratt((
-                infix(left(2), op("|>"), |l, r| Expr::Pipeline(Box::new(l), Box::new(r))),
-                postfix(1, op("->>").ignore_then(ident), |l, r| Expr::Mutate(Box::new(l), r)),
-                postfix(1, op("->").ignore_then(ident), |l, r| Expr::Assign(Box::new(l), r)),
-                prefix(1, arg_list.padded().then_ignore(op("=>")), |l, r| Expr::Lambda(l, Box::new(r)))
+                infix(left(2), just(Token::Pipeline), |l, r| Expr::Pipeline(Box::new(l), Box::new(r))),
+                postfix(1, just(Token::Mutate).ignore_then(ident), |l, r| Expr::Mutate(Box::new(l), r)),
+                postfix(1, just(Token::Assign).ignore_then(ident), |l, r| Expr::Assign(Box::new(l), r)),
+                prefix(1, arg_list.then_ignore(just(Token::Lambda)), |l, r| Expr::Lambda(l, Box::new(r)))
             ))
     })
-        .lazy()
-        .then_ignore(end())
+       .lazy()
+}
+
+use ariadne::{Report, ReportKind, Label, Color, sources};
+
+fn show_errs<T: Clone + Display>(src: &str, filename: &'static str, errs: Vec<Rich<T>>) {
+    errs.into_iter()
+        .map(|e| e.map_token(|c| c.to_string()))
+        .for_each(|e| {
+            Report::build(ReportKind::Error, filename, e.span().start)
+                .with_message(e.to_string())
+                .with_label(
+                    Label::new((filename, e.span().into_range()))
+                        .with_message(format!("Expected: {:?}", e.expected().collect::<Vec<_>>()))
+                        .with_color(Color::Red),
+                )
+                .finish()
+                .print(sources([(filename, src)]))
+                .unwrap()
+        });
 }
 
 fn main() {
@@ -80,24 +158,18 @@ fn main() {
 
     let src = include_str!("../test.pipe");
     println!("{src}");
-    let (o, errs) = parser().parse(src).into_output_errors();
 
-    if let Some(output) = o {
-        println!("{output:#?}");
-    }
+    let (tokens, errs) = lexer().parse(src).into_output_errors();
 
-    errs.into_iter()
-        .map(|e| e.map_token(|c| c.to_string()))
-        .for_each(|e| {
-            Report::build(ReportKind::Error, FILENAME, e.span().start)
-                .with_message(e.to_string())
-                .with_label(
-                    Label::new((FILENAME, e.span().into_range()))
-                        .with_message(format!("Expected: {:?}", e.expected().collect::<Vec<_>>()))
-                        .with_color(Color::Red),
-                )
-                .finish()
-                .print(sources([(FILENAME, src.clone())]))
-                .unwrap()
-        });
+    show_errs(src, FILENAME, errs);
+
+    let Some(tokens) = tokens else { return };
+
+    let (ast, errs) = parser().parse(Input::spanned(&tokens, (src.len()..src.len()).into())).into_output_errors();
+
+    show_errs(src, FILENAME, errs);
+
+    let Some(ast) = ast else { return };
+
+    println!("{ast:#?}");
 }
